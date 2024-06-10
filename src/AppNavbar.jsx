a7ad11e42f10904be2c30fbe45b51ff19f9d0818
sult <168359467+Salbdr@users.noreply.github.com>
2024-06-10 19:44:34 +0300

    test

---
+++ src/AppNavbar.jsx
@@ -0,0 +1,18 @@
+// import "./nav.css";
+export default function AppNavbar(){
+ return(
+  <div >
+    <ul style={{backgroundColor:"orange", display:"flex", justifyContent:"space-around", alignItems:"center", flexDirection:"row", height:"60px" }} >
+      <li>All catolgies</li>
+      <li>Tody Deals</li>
+      <li>Elctronic</li>
+      <li>signin</li>
+      <li>pages</li>
+      <li>pages</li>
+    </ul>
+
+    
+  </div>
+    
+ )
+}
